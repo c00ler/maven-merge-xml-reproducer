14a9f9ef0c8d81f6f55e81b36d8cedc32b3d8b34
Alexey Venderov <avenderov@gradle.com>
2023-02-06 14:57:30 +0100

    Compare maven-4-alpha2 to maven-4-alpha3

--- reproduce.sh
+++ reproduce.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-rm -rf maven3.log maven4.log
+rm -rf maven-4-alpha2.log maven-4-alpha-3.log
 
-cd maven-3
-./mvnw help:effective-pom > ../maven3.log
-cd ../maven-4
-./mvnw help:effective-pom > ../maven4.log
+cd maven-4-alpha2
+./mvnw help:effective-pom > ../maven-4-alpha2.log
+cd ../maven-4-alpha3
+./mvnw help:effective-pom > ../maven-4-alpha3.log
 cd ..
 
-diff maven3.log maven4.log
+diff maven-4-alpha2.log maven-4-alpha3.log
